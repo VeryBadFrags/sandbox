f40ab49b7330399092522ec8c9828dc9cfd61ef1
VeryBadFrags <1664052+VeryBadFrags@users.noreply.github.com>
2023-11-26 09:18:48 -0800

    Add base for a level parser

---
+++ src/levels/level_parser.ts
@@ -0,0 +1,19 @@
+import * as Game from "../game";
+import * as CellType from "../type/Cell";
+
+interface Level {
+  taps: Array<Object>;
+  tiles: Array<gameCell>;
+}
+
+interface gameCell {
+  x: number;
+  y: number;
+  cell: CellType.Cell;
+}
+
+export function parseLevel(level: Level) {
+  for (const tile of level.tiles) {
+    Game.createCell(tile.x, tile.y, tile.cell);
+  }
+}
